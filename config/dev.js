31e225bc43100a879bdc30075b9bbffb7bded231
Tin Chu <mchu.tinho@gmail.com>
2019-04-26 14:20:38 -0500

    Added Upload Image
    Signed-off-by: Tin Chu <mchu.tinho@gmail.com>

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://tin:password1@ds117866.mlab.com:17866/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAYERHZ7IZIPIBTDCW',
+  secretAccessKey: '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'
 };
